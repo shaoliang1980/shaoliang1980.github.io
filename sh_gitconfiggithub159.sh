c4a8b213b0a024514376c269a8d89d3cd1493103
shaoliang1980 <2090727@qq.com>
2019-05-19 21:33:04 +0800

    update by shao

---
+++ sh_gitconfiggithub159.sh
@@ -0,0 +1,12 @@
+#git config --global user.name "shaoliang1980" 
+
+#git config --global user.email "2090727@qq.com" 
+
+git config --global user.name "shaoyiping" 
+
+git config --global user.email "15953652221@163.com" 
+
+
+# git config --list
+
+
